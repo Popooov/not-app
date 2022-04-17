3e541ff43e9b6e9502c09acfb6b41f2d5e2d72e6
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2022-04-17 23:53:08 +0100

    Add border and border color to listbox

--- src/components/ScaleListBox.js
+++ src/components/ScaleListBox.js
@@ -11,7 +11,7 @@ export default function ScaleListBox({ scale, selected, setSelected }) {
         value={selected}
         onChange={setSelected}>
         <div className="relative mt-1">
-          <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left text-xs bg-white rounded-lg bg-zinc-100 cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
+          <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left text-xs rounded-lg bg-zinc-100 border-2 border-zinc-400 cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
             <span className="block truncate">{selected}</span>
             <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
               <SelectorIcon
