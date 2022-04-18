1f5a22b5be47d0acefb41540170e00cda3ee11df
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2022-04-18 03:52:07 +0100

    Change SSE state from useRef to useState

--- src/hooks/useEventSource.js
+++ src/hooks/useEventSource.js
@@ -1,21 +1,21 @@
 import { useState, useEffect, useRef } from "react"
 
-const eventSource = new EventSource('http://www.not.iac.es/observing/dyn/hiresdata.php')
+const sse = new EventSource('http://www.not.iac.es/observing/dyn/hiresdata.php')
 
 export const useEventSource = () => {
     const [ statusData, setStatusData ] = useState([])
     const [ enabled, setEnabled ] = useState(false)
-    // const [ eventSource, setEventSource ] = useState(new EventSource('http://www.not.iac.es/observing/dyn/hiresdata.php'))
-    const eventSourceRef = useRef(eventSource)
+    const [ eventSource ] = useState(sse)
+    // const eventSourceRef = useRef(eventSource)
 
     // console.log(eventSourceRef.current)
 
     useEffect(() => {
-        const sse = eventSourceRef.current
-        eventSourceRef.current.onmessage = (e) => setStatusData(JSON.parse(e.data))
-
+        // const sse = eventSourceRef.current
+        // eventSourceRef.current.onmessage = (e) => setStatusData(JSON.parse(e.data))
+        eventSource.onmessage = (e) => setStatusData(JSON.parse(e.data))
         return () => sse.close()
-    }, [])
+    }, [eventSource])
     // console.log(statusData)
 
     return { 
