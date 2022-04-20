cc2a4ae104cd4f15d62e6a6e59e21c220b378547
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2022-04-20 17:46:17 +0100

    Fix reset button padding

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ const Button = (props) => {
 
     return (
         <button 
-            className="mt-1 py-2 px-4 sm:py-[.4rem] sm:text-base rounded-lg text-center text-xs bg-zinc-100 border-2 border-zinc-400 cursor-pointer"
+            className="mt-1 py-2 px-4 sm:py-[.35rem] sm:text-base rounded-lg text-center text-xs bg-zinc-100 border-2 border-zinc-400 cursor-pointer"
             onClick={props.reset}>
                 Reset
         </button>
