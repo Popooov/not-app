57ad02f1c3b6371bf45cd7653abc183be742ccb9
Alexander <31711254+Popooov@users.noreply.github.com>
2022-11-11 10:45:27 +0000

    Change version

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,5 +1,5 @@
 export const Logo = () => {
     return (
-        <h1 className='text-xs sm:text-sm row-span-2 col-start-1 col-end-2 lg:text-base'>TCS Status v0.4.2</h1>
+        <h1 className='text-xs sm:text-sm row-span-2 col-start-1 col-end-2 lg:text-base'>TCS Status v0.4.3</h1>
     )
 }
